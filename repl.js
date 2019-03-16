7d8a8a310de8e3200e53e2288276f2b954392c5b
john.hennessey <john.hennessey@ef.com>
2019-03-15 20:49:31 -0400

    some cleanup

--- repl.js
+++ repl.js
@@ -8,44 +8,66 @@ const r = repl.start("λ ");
 r.defineCommand('watch', {
     help: 'Automatically reload file in repo on file change',
     action(file) {
-       this.clearBufferedCommand() 
-       watch(file, onChange)
+       const importFile = importIntoReplContext(r)
+
+       console.log(`importing: ${file}`)
+       importFile(file) 
+
+       this.clearBufferedCommand()
+       console.log(`Watching for changes to: ${file}`)
+
        this.displayPrompt()
+
+       watchFileForChange(file, () =>
+            {
+                console.log('reimporting module..')
+                importFile(file)
+                console.log('reimported module')
+                this.displayPrompt()
+            })
+
     }
 })
 
-function onChange(file) {
-    delete require.cache[require.resolve(file)]; 
-    console.log('reimporting')
-    const newImport = require(file)
-    r.context = vm.createContext(newImport)
+function importIntoReplContext(repl) {
+    return function(file) {
+        try {
+            delete require.cache[require.resolve(file)];
+            const newImport = require(file)
+            repl.context = vm.createContext(newImport)
+        } catch(e) {
+            throw new Error(`Unable to load ${file} into context`)
+        }
+    }
 }
 
-function watch(file, onChange) {
+function watchFileForChange(file, onChange) {
     if (!file) throw new Error('file missing')
-    console.log(`Watching for changes to: ${file}`)
 
     let md5Previous = null
     let fsWait = false
 
     fs.watch(file, (event, fileName) => {
-        if (fileName) {
-            if (fsWait) return
+        try {
+            if (fileName) {
+                if (fsWait) return
 
-            fsWait = setTimeout(() => {
-                fsWait = false
-            }, 100)
+                fsWait = setTimeout(() => {
+                    fsWait = false
+                }, 100)
 
-            console.log(`${file}: getting latest`)
-            const md5Current = md5(fs.readFileSync(file))
-            
-            if (md5Current === md5Previous) {
-                return
+                console.log(`${file}: getting latest`)
+                const md5Current = md5(fs.readFileSync(file))
+
+                if (md5Current === md5Previous) {
+                    return
+                }
+
+                md5Previous = md5Current
+                onChange()
             }
-
-            md5Previous = md5Current
-            console.log(`${file} changed`)
-            onChange(file)
+        } catch(e) {
+            throw new Error(`Error occured when watching file: ${file}`)
         }
     })
 }
